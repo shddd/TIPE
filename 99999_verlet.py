9e3ff4ff86482fe5f12168b0b01af9f7c7aefe5f
shddd <61633436+shddd@users.noreply.github.com>
2023-01-16 13:56:33 +0100

    Update 99999_verlet.py

--- 99999_verlet.py
+++ 99999_verlet.py
@@ -12,3 +12,18 @@ def verlet_method(M, F, u0, v0, dt, n_steps):
         v[i] = (u[i]-u[i-2])/(2*dt)
         a[i] = (u[i]-2*u[i-1]+u[i-2])/(dt**2)
     return a, v, u
+
+
+import matplotlib.pyplot as plt
+
+beta_values = np.linspace(0, 1, 50)
+gamma_values = np.linspace(0, 1, 50)
+
+for beta in beta_values:
+    for gamma in gamma_values:
+        _, _, u = newmark_method(M, C, K, F, u0, v0, dt, n_steps, beta, gamma)
+        plt.plot(u, label='beta={:.2f}, gamma={:.2f}'.format(beta, gamma))
+
+plt.title("Position en fonction de beta et gamma")
+plt.legend()
+plt.show()
