592ad0c9b206504c47e1da0810d56379b78b6f17
shddd <61633436+shddd@users.noreply.github.com>
2023-01-16 13:48:46 +0100

    Update 99999_verlet.py

--- 99999_verlet.py
+++ 99999_verlet.py
@@ -3,10 +3,12 @@ import numpy as np
 def verlet_method(M, F, u0, v0, dt, n_steps):
     u = np.zeros(n_steps+1)
     v = np.zeros(n_steps+1)
+    a = np.zeros(n_steps+1)
     u[0] = u0
     v[0] = v0
-    a = (F/M)
-    for i in range(1, n_steps+1):
-        u[i] = 2*u[i-1] - u[i-2] + a*(dt**2)
+    a[0] = F/M
+    for i in range(1, n_steps):
+        u[i] = 2*u[i-1] - u[i-2] + a[i-1]*(dt**2)
         v[i] = (u[i]-u[i-2])/(2*dt)
-    return v, u
+        a[i] = (u[i]-2*u[i-1]+u[i-2])/(dt**2)
+    return a, v, u
