06e2e1336a8ae95a6dd859944511f7106872b27e
shddd <61633436+shddd@users.noreply.github.com>
2023-01-16 13:44:26 +0100

    Update 99999_EE.py

--- 99999_EE.py
+++ 99999_EE.py
@@ -3,10 +3,11 @@ import numpy as np
 def explicit_euler_method(M, C, K, F, u0, v0, dt, n_steps):
     u = np.zeros(n_steps+1)
     v = np.zeros(n_steps+1)
+    a = np.zeros(n_steps+1)
     u[0] = u0
     v[0] = v0
     for i in range(1, n_steps+1):
-        a = (F - C*v[i-1] - K*u[i-1])/M
-        v[i] = v[i-1] + dt*a
+        a[i] = (F - C*v[i-1] - K*u[i-1])/M
+        v[i] = v[i-1] + dt*a[i]
         u[i] = u[i-1] + dt*v[i-1]
-    return v, u
+    return a, v, u
